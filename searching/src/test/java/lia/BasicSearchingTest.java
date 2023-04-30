bd2509a8c7914cced3b37f44779ed216aa050f69
Usman Shaikh <ushaikh@sonatype.com>
2023-04-30 19:11:02 +0100

    polish code - basic searching test

--- searching/src/test/java/lia/BasicSearchingTest.java
+++ searching/src/test/java/lia/BasicSearchingTest.java
@@ -11,54 +11,62 @@ import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BasicSearchingTest
 {
+  private Directory directory;
+
+  private IndexSearcher searcher;
+
+  @BeforeEach
+  void setup() throws Exception {
+    directory = TestUtil.getBookIndexDirectory();
+    searcher = new IndexSearcher(DirectoryReader.open(directory));
+  }
+
+  @AfterEach
+  void tearDown() throws Exception {
+    directory.close();
+  }
+
   @Test
   void testTerm() throws Exception {
-    Directory dir = TestUtil.getBookIndexDirectory();
-    IndexSearcher searcher = new IndexSearcher(DirectoryReader.open(dir));
-
-    Term t = new Term("subject", "ant");
-    Query query = new TermQuery(t);
+    Term term = new Term("subject", "ant");
+    Query query = new TermQuery(term);
 
     TopDocs docs = searcher.search(query, 10);
     assertThat(docs.totalHits.value).isOne();
 
-    t = new Term("subject", "junit");
-    docs = searcher.search(new TermQuery(t), 10);
+    term = new Term("subject", "junit");
+    docs = searcher.search(new TermQuery(term), 10);
     assertThat(docs.totalHits.value).isEqualTo(2);
-    dir.close();
   }
 
   @Test
   void testKeyword() throws Exception {
-    Directory dir = TestUtil.getBookIndexDirectory();
-    IndexSearcher searcher = new IndexSearcher(DirectoryReader.open(dir));
-    Term t = new Term("isbn", "9781935182023");
-    Query query = new TermQuery(t);
+    Term term = new Term("isbn", "9781935182023");
+    Query query = new TermQuery(term);
     TopDocs docs = searcher.search(query, 10);
     assertThat(docs.totalHits.value).isOne();
-    dir.close();
   }
 
   @Test
   void testQueryParser() throws Exception {
-    Directory dir = TestUtil.getBookIndexDirectory();
-    IndexSearcher searcher = new IndexSearcher(DirectoryReader.open(dir));
-
     QueryParser parser = new QueryParser("contents", new StandardAnalyzer());
     Query query = parser.parse("+JUNIT +ANT -MOCK");
     TopDocs docs = searcher.search(query, 10);
     assertThat(docs.totalHits.value).isOne();
-    Document d = searcher.storedFields().document(docs.scoreDocs[0].doc);
-    assertThat(d.get("title")).isEqualTo("Ant in Action");
+
+    Document doc = searcher.storedFields().document(docs.scoreDocs[0].doc);
+    assertThat(doc.get("title")).isEqualTo("Ant in Action");
+
     query = parser.parse("mock OR junit");
     docs = searcher.search(query, 10);
     assertThat(docs.totalHits.value).isEqualTo(2);
-    dir.close();
   }
 }
