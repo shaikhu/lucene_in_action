0a86be64e373fa84876e35e8315890559dbcf423
Usman Shaikh <ushaikh@sonatype.com>
2023-04-30 19:31:55 +0100

    polish code

--- searching/src/main/java/lia/Explainer.java
+++ searching/src/main/java/lia/Explainer.java
@@ -14,31 +14,29 @@ import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
-public class Explainer
-{
-  public static void main(String[] args) throws Exception {
+public class Explainer {
+  public static void main(String... args) throws Exception {
     if (args.length != 2) {
-      System.err.println("Usage: Explainer <index dir> <query>");
-      System.exit(1);
+      throw new IllegalArgumentException("Usage: Explainer <index dir> <query>");
     }
 
     String indexDir = args[0];
     String queryExpression = args[1];
 
-    Directory directory = FSDirectory.open(Paths.get(indexDir));
-    QueryParser parser = new QueryParser("contents", new StandardAnalyzer());
-    Query query = parser.parse(queryExpression);
+    try (Directory directory = FSDirectory.open(Paths.get(indexDir))) {
+      QueryParser parser = new QueryParser("contents", new StandardAnalyzer());
+      Query query = parser.parse(queryExpression);
 
-    System.out.println("Query: " + queryExpression);
-    IndexSearcher searcher = new IndexSearcher(DirectoryReader.open(directory));
-    TopDocs topDocs = searcher.search(query, 10);
-    for (ScoreDoc match : topDocs.scoreDocs) {
-      Explanation explanation = searcher.explain(query, match.doc);
-      System.out.println("----------");
-      Document doc = searcher.storedFields().document(match.doc);
-      System.out.println(doc.get("title"));
-      System.out.println(explanation.toString());
+      System.out.println("Query: " + queryExpression);
+      IndexSearcher searcher = new IndexSearcher(DirectoryReader.open(directory));
+      TopDocs topDocs = searcher.search(query, 10);
+      for (ScoreDoc match : topDocs.scoreDocs) {
+        Explanation explanation = searcher.explain(query, match.doc);
+        System.out.println("----------");
+        Document doc = searcher.storedFields().document(match.doc);
+        System.out.println(doc.get("title"));
+        System.out.println(explanation.toString());
+      }
     }
-    directory.close();
   }
 }
