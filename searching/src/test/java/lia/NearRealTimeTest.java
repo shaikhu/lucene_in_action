86b8da660d86aded920cec7ad6f0a8b45e705752
Usman Shaikh <ushaikh@sonatype.com>
2023-04-30 19:23:07 +0100

    polish code - near real time test

--- searching/src/test/java/lia/NearRealTimeTest.java
+++ searching/src/test/java/lia/NearRealTimeTest.java
@@ -23,41 +23,41 @@ public class NearRealTimeTest
 {
   @Test
   void testNearRealTime() throws Exception {
-    Directory directory = new ByteBuffersDirectory();
-    IndexWriter writer = new IndexWriter(directory, new IndexWriterConfig(new StandardAnalyzer()));
+    try (Directory directory = new ByteBuffersDirectory();
+         IndexWriter writer = new IndexWriter(directory, new IndexWriterConfig(new StandardAnalyzer()))) {
 
-    for (int i = 0; i < 10; i++) {
+      for (int i = 0; i < 10; i++) {
+        Document doc = new Document();
+        doc.add(new StringField("id", String.valueOf(i), Store.NO));
+        doc.add(new TextField("text", "aaa", Store.NO));
+        writer.addDocument(doc);
+      }
+
+      DirectoryReader reader = DirectoryReader.open(writer);
+      IndexSearcher searcher = new IndexSearcher(reader);
+
+      Query query = new TermQuery(new Term("text", "aaa"));
+      TopDocs docs = searcher.search(query, 1);
+      assertThat(docs.totalHits.value).isEqualTo(10);
+
+      writer.deleteDocuments(new Term("id", "7"));
+
       Document doc = new Document();
-      doc.add(new StringField("id", String.valueOf(i), Store.NO));
-      doc.add(new TextField("text", "aaa", Store.NO));
+      doc.add(new StringField("id", "11", Store.NO));
+      doc.add(new TextField("text", "bbb", Store.NO));
       writer.addDocument(doc);
+
+      DirectoryReader newReader = DirectoryReader.openIfChanged(reader);
+      assertThat(reader).isNotSameAs(newReader);
+      reader.close();
+      searcher = new IndexSearcher(newReader);
+
+      TopDocs hits = searcher.search(query, 10);
+      assertThat(hits.totalHits.value).isEqualTo(9);
+      query = new TermQuery(new Term("text", "bbb"));
+      hits = searcher.search(query, 1);
+      assertThat(hits.totalHits.value).isOne();
+      newReader.close();
     }
-
-    DirectoryReader reader = DirectoryReader.open(writer);
-    IndexSearcher searcher = new IndexSearcher(reader);
-
-    Query query = new TermQuery(new Term("text", "aaa"));
-    TopDocs docs = searcher.search(query, 1);
-    assertThat(docs.totalHits.value).isEqualTo(10);
-
-    writer.deleteDocuments(new Term("id", "7"));
-
-    Document doc = new Document();
-    doc.add(new StringField("id", "11", Store.NO));
-    doc.add(new TextField("text", "bbb", Store.NO));
-    writer.addDocument(doc);
-
-    DirectoryReader newReader = DirectoryReader.openIfChanged(reader);
-    assertThat(reader).isNotSameAs(newReader);
-    reader.close();
-    searcher = new IndexSearcher(newReader);
-
-    TopDocs hits = searcher.search(query, 10);
-    assertThat(hits.totalHits.value).isEqualTo(9);
-    query = new TermQuery(new Term("text", "bbb"));
-    hits = searcher.search(query, 1);
-    assertThat(hits.totalHits.value).isOne();
-    newReader.close();
-    directory.close();
   }
 }
