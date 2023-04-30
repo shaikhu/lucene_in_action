901612551df02eacdd9bb79da9b8d104a1f09554
Usman Shaikh <ushaikh@sonatype.com>
2023-04-30 18:45:53 +0100

    polish code - indexing test

--- indexing/src/test/java/lia/IndexingTest.java
+++ indexing/src/test/java/lia/IndexingTest.java
@@ -12,7 +12,6 @@ import org.apache.lucene.document.Field.Store;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.Term;
@@ -21,13 +20,13 @@ import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.store.ByteBuffersDirectory;
 import org.apache.lucene.store.Directory;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class IndexingTest
-{
+public class IndexingTest {
   private static final List<String> IDS = Arrays.asList("1", "2");
 
   private static final List<String> UNINDEXED = Arrays.asList("Netherlands", "Italy");
@@ -54,61 +53,51 @@ public class IndexingTest
     }
   }
 
-  private IndexWriter getWriter() throws IOException {
-    return new IndexWriter(directory, new IndexWriterConfig(new WhitespaceAnalyzer()));
-  }
-
-  private long getHitCount(String fieldName, String searchString) throws IOException {
-    IndexReader reader = DirectoryReader.open(directory);
-    IndexSearcher searcher = new IndexSearcher(reader);
-    Term t = new Term(fieldName, searchString);
-    Query query = new TermQuery(t);
-    long hitCount = TestUtil.hitCount(searcher, query);
-    reader.close();
-    return hitCount;
+  @AfterEach
+  void tearDown() throws Exception {
+    directory.close();
   }
 
   @Test
   void testIndexWriter() throws IOException {
-    IndexWriter writer = getWriter();
-    assertThat(writer.getDocStats().numDocs).isEqualTo(IDS.size());
-    writer.close();
+    try (IndexWriter writer = getWriter()) {
+      assertThat(writer.getDocStats().numDocs).isEqualTo(IDS.size());
+    }
   }
 
   @Test
   void testIndexReader() throws IOException {
-    IndexReader reader = DirectoryReader.open(directory);
-    assertThat(reader.maxDoc()).isEqualTo(IDS.size());
-    assertThat(reader.numDocs()).isEqualTo(IDS.size());
-    reader.close();
+    try (DirectoryReader reader = DirectoryReader.open(directory)) {
+      assertThat(reader.maxDoc()).isEqualTo(IDS.size());
+      assertThat(reader.numDocs()).isEqualTo(IDS.size());
+    }
   }
 
   @Test
   void testDelete() throws IOException {
-    IndexWriter writer = getWriter();
-    assertThat(writer.getDocStats().numDocs).isEqualTo(2);
-    writer.deleteDocuments(new Term("id", "1"));
-    writer.commit();
-    assertThat(writer.hasDeletions()).isTrue();
+    try (IndexWriter writer = getWriter()) {
+      assertThat(writer.getDocStats().numDocs).isEqualTo(2);
+      writer.deleteDocuments(new Term("id", "1"));
+      writer.commit();
+      assertThat(writer.hasDeletions()).isTrue();
 
-    assertThat(writer.getDocStats().maxDoc).isEqualTo(2);
-    assertThat(writer.getDocStats().numDocs).isOne();
-
-    writer.close();
+      assertThat(writer.getDocStats().maxDoc).isEqualTo(2);
+      assertThat(writer.getDocStats().numDocs).isOne();
+    }
   }
 
   @Test
   void testUpdate() throws IOException {
     assertThat(getHitCount("city", "Amsterdam")).isOne();
 
-    IndexWriter writer = getWriter();
-    Document document = new Document();
-    document.add(new StringField("id", "1", Store.YES));
-    document.add(new StringField("country", "Netherlands", Store.YES));
-    document.add(new TextField("contents", "Den Haag has a lot of museums", Store.NO));
-    document.add(new StringField("city", "Den Haag", Store.YES));
-    writer.updateDocument(new Term("id", "1"), document);
-    writer.close();
+    try (IndexWriter writer = getWriter()) {
+      Document document = new Document();
+      document.add(new StringField("id", "1", Store.YES));
+      document.add(new StringField("country", "Netherlands", Store.YES));
+      document.add(new TextField("contents", "Den Haag has a lot of museums", Store.NO));
+      document.add(new StringField("city", "Den Haag", Store.YES));
+      writer.updateDocument(new Term("id", "1"), document);
+    }
 
     assertThat(getHitCount("city", "Amsterdam")).isZero();
     assertThat(getHitCount("city", "Den Haag")).isOne();
@@ -117,14 +106,25 @@ public class IndexingTest
   @Test
   void testMaxFieldLength() throws IOException {
     assertThat(getHitCount("contents", "bridges")).isOne();
-    IndexWriter writer =
-        new IndexWriter(directory, new IndexWriterConfig(new LimitTokenCountAnalyzer(new WhitespaceAnalyzer(), 1)));
-
-    Document doc = new Document();
-    doc.add(new TextField("contents", "these bridges can't be found", Store.NO));
-    writer.addDocument(doc);
-    writer.close();
-
+    try (IndexWriter writer = new IndexWriter(directory,
+        new IndexWriterConfig(new LimitTokenCountAnalyzer(new WhitespaceAnalyzer(), 1)))) {
+      Document doc = new Document();
+      doc.add(new TextField("contents", "these bridges can't be found", Store.NO));
+      writer.addDocument(doc);
+    }
     assertThat(getHitCount("contents", "bridges")).isOne();
+  }
+
+  private IndexWriter getWriter() throws IOException {
+    return new IndexWriter(directory, new IndexWriterConfig(new WhitespaceAnalyzer()));
+  }
+
+  private long getHitCount(String fieldName, String searchString) throws IOException {
+    try (DirectoryReader reader = DirectoryReader.open(directory)) {
+      IndexSearcher searcher = new IndexSearcher(reader);
+      Term term = new Term(fieldName, searchString);
+      Query query = new TermQuery(term);
+      return TestUtil.hitCount(searcher, query);
+    }
   }
 }
