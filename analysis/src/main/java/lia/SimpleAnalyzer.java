70d46ce56a45d772e38a5522df114755543d4633
Usman Shaikh <ushaikh@sonatype.com>
2023-05-02 07:32:34 +0100

    formatting

--- analysis/src/main/java/lia/SimpleAnalyzer.java
+++ analysis/src/main/java/lia/SimpleAnalyzer.java
@@ -8,9 +8,7 @@ import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.Tokenizer;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
 
-public class SimpleAnalyzer extends Analyzer
-{
-
+public class SimpleAnalyzer extends Analyzer {
   @Override
   protected TokenStreamComponents createComponents(final String fieldName) {
     Tokenizer tokenizer = new StandardTokenizer();
