fab6b2e9233ee2fb114dfc3d22d73126ef799402
Usman Shaikh <ushaikh@sonatype.com>
2023-05-15 07:17:41 +0100

    use try with resources

--- tools/src/test/java/lia/RegexQueryTest.java
+++ tools/src/test/java/lia/RegexQueryTest.java
@@ -11,18 +11,18 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class RegexQueryTest
-{
+public class RegexQueryTest {
   @Test
   void testRegexQuery() throws Exception {
-    Directory directory = TestUtil.getBookIndexDirectory();
-    IndexSearcher searcher = new IndexSearcher(DirectoryReader.open(directory));
-    RegexpQuery query = new RegexpQuery(new Term("title", ".*st.*"));
-    TopDocs hits = searcher.search(query, 10);
-    assertThat(hits.totalHits.value).isEqualTo(2);
-    assertThat(TestUtil.hitsIncludeTitle(searcher, hits, "Tapestry in Action")).isTrue();
-    assertThat(
-        TestUtil.hitsIncludeTitle(searcher, hits, "Mindstorms: Children, Computers, And Powerful Ideas")).isTrue();
-    directory.close();
+    try (Directory directory = TestUtil.getBookIndexDirectory()) {
+      IndexSearcher searcher = new IndexSearcher(DirectoryReader.open(directory));
+      RegexpQuery query = new RegexpQuery(new Term("title", ".*st.*"));
+      TopDocs hits = searcher.search(query, 10);
+
+      assertThat(hits.totalHits.value).isEqualTo(2);
+      assertThat(TestUtil.hitsIncludeTitle(searcher, hits, "Tapestry in Action")).isTrue();
+      assertThat(
+          TestUtil.hitsIncludeTitle(searcher, hits, "Mindstorms: Children, Computers, And Powerful Ideas")).isTrue();
+    }
   }
 }
