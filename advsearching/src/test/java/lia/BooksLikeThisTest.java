beeb97b71db126b6953c902d1b3528fea1225224
Usman Shaikh <ushaikh@sonatype.com>
2023-05-16 17:25:14 +0100

    fix spelling error

--- advsearching/src/test/java/lia/BooksLikeThisTest.java
+++ advsearching/src/test/java/lia/BooksLikeThisTest.java
@@ -21,7 +21,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class BooksLIkeThisTest {
+public class BooksLikeThisTest {
   private Directory directory;
 
   private DirectoryReader reader;
