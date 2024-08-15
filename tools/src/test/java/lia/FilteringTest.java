9b6c30c8b57e771fe17c1611bd2f5b2a82bfbb65
Usman Shaikh <shaikhu@gmail.com>
2024-08-15 09:42:22 +0100

    tidy up filtering test

--- tools/src/test/java/lia/FilteringTest.java
+++ tools/src/test/java/lia/FilteringTest.java
@@ -20,7 +20,6 @@ import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TermRangeQuery;
-import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.ByteBuffersDirectory;
 import org.apache.lucene.store.Directory;
 import org.junit.jupiter.api.AfterEach;
@@ -34,15 +33,12 @@ class FilteringTest {
 
   private static final LocalDateTime START_DATE = LocalDateTime.of(2009, Month.FEBRUARY, 1, 0, 0);
 
-  private static final Query SUE_TERM_QUERY = new TermQuery(new Term("owner", "sue"));
+  private static final Query TERM_QUERY_SUE = new TermQuery(new Term("owner", "sue"));
 
-  private static final Query BOB_TERM_QUERY = new TermQuery(new Term("owner", "bob"));
+  private static final Query TERM_QUERY_BOB = new TermQuery(new Term("owner", "bob"));
 
   private static final Query DATE_QUERY = TermRangeQuery.newStringRange("date",
-      String.valueOf(Long.MIN_VALUE),
-      String.valueOf(Long.MAX_VALUE),
-      true,
-      true);
+      String.valueOf(Long.MIN_VALUE), String.valueOf(Long.MAX_VALUE), true, true);
 
   private Directory directory;
 
@@ -55,11 +51,11 @@ class FilteringTest {
     var date = START_DATE;
     try (var indexWriter = new IndexWriter(directory, new IndexWriterConfig(new WhitespaceAnalyzer()))) {
       for (var i = 0; i < MAX; i++) {
-        var doc = new Document();
-        doc.add(new StringField("key", String.valueOf(i + 1), Store.YES));
-        doc.add(new StringField("owner", (i < MAX / 2) ? "bob" : "sue", Store.YES));
-        doc.add(new StringField("date", DateTools.timeToString(date.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli(), Resolution.DAY), Store.YES));
-        indexWriter.addDocument(doc);
+        var document = new Document();
+        document.add(new StringField("key", String.valueOf(i + 1), Store.YES));
+        document.add(new StringField("owner", (i < MAX / 2) ? "bob" : "sue", Store.YES));
+        document.add(new StringField("date", DateTools.timeToString(date.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli(), Resolution.DAY), Store.YES));
+        indexWriter.addDocument(document);
         date = date.plusDays(1);
       }
     }
@@ -75,8 +71,8 @@ class FilteringTest {
   @Test
   void testOr() throws Exception {
     var query = new BooleanQuery.Builder()
-        .add(SUE_TERM_QUERY, Occur.SHOULD)
-        .add(BOB_TERM_QUERY, Occur.SHOULD)
+        .add(TERM_QUERY_SUE, Occur.SHOULD)
+        .add(TERM_QUERY_BOB, Occur.SHOULD)
         .build();
 
     var topDocs = indexSearcher.search(query, 10);
@@ -87,7 +83,7 @@ class FilteringTest {
   void testAnd() throws Exception {
     var query = new BooleanQuery.Builder()
         .add(DATE_QUERY, Occur.MUST)
-        .add(BOB_TERM_QUERY, Occur.MUST)
+        .add(TERM_QUERY_BOB, Occur.MUST)
         .build();
 
     var topDocs = indexSearcher.search(query, 10);
@@ -101,7 +97,7 @@ class FilteringTest {
   void testAndNot() throws Exception {
     var query = new BooleanQuery.Builder()
         .add(DATE_QUERY, Occur.MUST)
-        .add(BOB_TERM_QUERY, Occur.MUST_NOT)
+        .add(TERM_QUERY_BOB, Occur.MUST_NOT)
         .build();
 
     var topDocs = indexSearcher.search(query, 10);
