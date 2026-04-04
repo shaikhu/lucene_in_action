24b63c7b267a0d6423b30699f32a1263d7868348
Usman Shaikh <shaikhu@gmail.com>
2026-04-04 17:10:17 +0100

    refactor: use Java virtual threads

--- admin/src/main/java/lia/ThreadedIndexWriter.java
+++ admin/src/main/java/lia/ThreadedIndexWriter.java
@@ -1,10 +1,8 @@
 package lia;
 
 import java.io.IOException;
-import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Executors;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.document.Document;
@@ -14,81 +12,42 @@ import org.apache.lucene.index.Term;
 import org.apache.lucene.store.Directory;
 
 public class ThreadedIndexWriter extends IndexWriter {
-  private ExecutorService threadPool;
+  // Virtual threads are cheap JVM-managed threads
+  private final ExecutorService threadPool = Executors.newVirtualThreadPerTaskExecutor();
 
-  private Analyzer defaultAnalyzer;
-
-  public ThreadedIndexWriter(Directory directory, Analyzer analyzer, int numThreads, int maxQueueSize) throws IOException {
+  public ThreadedIndexWriter(Directory directory, Analyzer analyzer) throws IOException {
     super(directory, new IndexWriterConfig(analyzer));
-    defaultAnalyzer = analyzer;
-    threadPool = new ThreadPoolExecutor(numThreads, numThreads, 0, TimeUnit.SECONDS,
-        new ArrayBlockingQueue<>(maxQueueSize, false), new ThreadPoolExecutor.CallerRunsPolicy());
   }
 
   public void addDocument(Document document) {
-    threadPool.execute(new Job(document, null, defaultAnalyzer));
-  }
-
-  public void addDocument(Document document, Analyzer analyzer) {
-    threadPool.execute(new Job(document, null, analyzer));
+    threadPool.execute(() -> {
+      try {
+        ThreadedIndexWriter.super.addDocument(document);
+      } catch (IOException e) {
+        throw new RuntimeException("Failed to add document in background thread", e);
+      }
+    });
   }
 
   public void updateDocument(Term term, Document document) {
-    threadPool.execute(new Job(document, term, defaultAnalyzer));
+    threadPool.execute(() -> {
+      try {
+        ThreadedIndexWriter.super.updateDocument(term, document);
+      } catch (IOException e) {
+        throw new RuntimeException("Failed to update document in background thread", e);
+      }
+    });
   }
 
-  public void updateDocument(Term term, Document document, Analyzer analyzer) {
-    threadPool.execute(new Job(document, term, analyzer));
-  }
-
+  @Override
   public void close() throws IOException {
-    finish();
+    threadPool.close(); // drain all pending tasks before committing
     super.close();
   }
 
+  @Override
   public void rollback() throws IOException {
-    finish();
+    threadPool.close(); // drain all pending tasks before discarding changes
     super.rollback();
-  }
-
-  private void finish() {
-    threadPool.shutdown();
-    while (true) {
-      try {
-        if (threadPool.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS)) {
-          break;
-        }
-      }
-      catch (InterruptedException ie) {
-        Thread.currentThread().interrupt();
-        throw new RuntimeException("Thread interrupted while waiting for index operations to complete", ie);
-      }
-    }
-  }
-
-  private class Job implements Runnable {
-    Document document;
-
-    Analyzer analyzer;
-
-    Term delTerm;
-
-    public Job(Document document, Term delTerm, Analyzer analyzer) {
-      this.document = document;
-      this.analyzer = analyzer;
-      this.delTerm = delTerm;
-    }
-
-    public void run() {
-      try {
-        if (delTerm != null) {
-          ThreadedIndexWriter.super.updateDocument(delTerm, document);
-        } else {
-          ThreadedIndexWriter.super.addDocument(document);
-        }
-      } catch (IOException ioe) {
-        throw new RuntimeException("Failed to execute index operation in background thread", ioe);
-      }
-    }
   }
 }
